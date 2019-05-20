a3247dbb52ab21a8350735c7a8509d2bd89d3f0a
Max Bosche <max@enten.space>
2019-05-20 20:32:07 +0200

    complete rework with intent_builder

--- __init__.py
+++ __init__.py
@@ -1,13 +1,19 @@
 import soco
 
-from mycroft import MycroftSkill, intent_file_handler
+from adapt.intent import IntentBuilder
+from mycroft import MycroftSkill, intent_handler
+# from mycroft.util.parse import extract_number
 from mycroft.util.log import getLogger
 from mycroft.util.log import LOG
 
 LOGGER = getLogger(__name__)
-VOL_STEP = 5
-
 class SonosControl(MycroftSkill):
+
+    MIN_LEVEL = 0
+    DEFAULT_LEVEL = 2
+    MAX_LEVEL = 100
+    LEVEL_STEP = 10
+
     def __init__(self):
         MycroftSkill.__init__(self)
 
@@ -18,89 +24,75 @@ class SonosControl(MycroftSkill):
             return
         LOG.debug("Found Sonos speakers")
 
-        members = {}
-        for speaker in speakers:
-            members[speaker.player_name.lower()] = speaker
-
-        self.members = members
-
-    @intent_file_handler('play.intent')
-    def handle_sonos_play_intent(self, message):
-        speaker = get_speaker(message.data.get("speaker"))
-
-        if speaker == None:
-            return
-
+    @intent_handler(IntentBuilder("SonosPlay").require("Sonos").require(
+        "Play"))
+    def handle_sonos_play(self, message):
+        speaker = self.__get_speaker(message)
         speaker.play()
+        self.speak_dialog('sonos.play')
 
-        self.speak_dialog("sonos.play")
-
-    @intent_file_handler('pause.intent')
-    def handle_sonos_pause_intent(self, message):
-        speaker = get_speaker(message.data.get("speaker"))
-
-        if speaker == None:
-            return
-
+    @intent_handler(IntentBuilder("SonosPause").require("Sonos").require(
+        "Pause"))
+    def handle_sonos_pause(self, message):
+        speaker = self.__get_speaker(message)
         speaker.pause()
-
-        self.speak_dialog("sonos.pause")
-
-
-    @intent_file_handler('volume_up.intent')
-    def handle_sonos_volume_up_intent(self, message):
-        speaker = get_speaker(message.data.get("speaker"))
-
-        if speaker == None:
-            return
+        self.speak_dialog('sonos.pause')
 
 
-        speaker.volume = speaker.volume + VOL_STEP
-        self.speak_dialog("sonos.volume_up")
-
-    @intent_file_handler('volume_down.intent')
-    def handle_sonos_volume_down_intent(self, message):
-        speaker = get_speaker(message.data.get("speaker"))
-
-        if speaker == None:
-            return
-
-
-        speaker.volume = speaker.volume - VOL_STEP
-        self.speak_dialog("sonos.volume_down")
-
-    @intent_file_handler('next_track.intent')
-    def handle_sonos_next_track_intent(self, message):
-        speaker = get_speaker(message.data.get("speaker"))
-
-        if speaker == None:
-            return
-
+    @intent_handler(IntentBuilder("NextTrack").require("Sonos").require(
+        "Next").optionally("Track"))
+    def handle_next_track(self, message):
+        speaker = self.__get_speaker(message)
         speaker.next()
+        self.speak_dialog('track.next')
 
-
-    @intent_file_handler('prev_track.intent')
-    def handle_sonos_prev_track_intent(self, message):
-        speaker = get_speaker(message.data.get("speaker"))
-
-        if speaker == None:
-            return
-
+    @intent_handler(IntentBuilder("PreviousTrack").require("Sonos").require(
+        "Previous").optionally("Track"))
+    def handle_previous_track(self, message):
+        speaker = self.__get_speaker(message)
         speaker.previous()
+        self.speak_dialog('track.previous')
 
-def get_speaker(utt):
-    if utt == None:
-        LOG.info("Undefined Speaker")
+    @intent_handler(IntentBuilder("SetVolume").require("Sonos").require(
+        "Volume").require("Level").optionally(
+        "Increase").optionally("Decrease"))
+    def handle_set_volume(self, message):
+        speaker = self.__get_speaker(message)
+        level = self.__get_volume_level(message)
+        self._setvolume(level, speaker)
+        self.speak_dialog('set.volume', data={'volume': int(level / 10)})
+
+    def _setvolume(self, vol, speaker):
+        speaker.volume = vol
+
+
+    @staticmethod
+    def __bound_level(level):
+        if level > SonosControl.MAX_LEVEL:
+            level = SonosControl.MAX_LEVEL
+        elif level < SonosControl.MIN_LEVEL:
+            level = SonosControl.MIN_LEVEL
+        return level
+
+    def __get_volume_level(self, message, default=DEFAULT_LEVEL):
+        level_str = message.data.get('Level', default)
+        level = default
+
+        try:
+            level = int(level_str)
+        except ValueError:
+            pass
+
+        level = self.__bound_level(level)
+        level = level * self.LEVEL_STEP
+        return level
+
+    def __get_speaker(self, message):
+        # TODO: Get Speaker by name
+        # TODO: Work with Groups
         speaker = soco.discovery.any_soco()
-    else:
-        LOG.info("Defined Speaker")
-        speaker = SonosControl.members.get(utt)
 
-    if speaker == None:
-        SonosControl.speak_dialog("sonos.nospeaker")
-        return None
-
-    return speaker
+        return speaker
 
 def create_skill():
     return SonosControl()
